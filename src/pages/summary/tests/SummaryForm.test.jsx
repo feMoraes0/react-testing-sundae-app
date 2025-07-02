23a81e77ad84c33a250c249475141af4f0984f4b
feMoraes0 <fernandomoraes.lopes@gmail.com>
2025-07-02 16:31:32 +0100

    replaces fireEvent with userEvent

--- src/pages/summary/tests/SummaryForm.test.jsx
+++ src/pages/summary/tests/SummaryForm.test.jsx
@@ -1,4 +1,5 @@
-import { render, fireEvent, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import SummaryForm from "../SummaryForm";
 
 describe("SummaryForm unit tests", () => {
@@ -10,14 +11,15 @@ describe("SummaryForm unit tests", () => {
     expect(buttonElement).toBeDisabled();
   });
 
-  it("Button should be enabled when checkbox is clicked and return to initial state when clicked second time", () => {
+  it("Button should be enabled when checkbox is clicked and return to initial state when clicked second time", async () => {
+    const user = userEvent.setup();
     render(<SummaryForm />);
     const checkboxElement = screen.getByRole("checkbox", { name: /terms and conditions/i });
     const buttonElement = screen.getByRole("button", { name: /confirm order/i });
-    fireEvent.click(checkboxElement);
+    await user.click(checkboxElement);
     expect(checkboxElement).toBeChecked();
     expect(buttonElement).toBeEnabled();
-    fireEvent.click(checkboxElement);
+    await user.click(checkboxElement);
     expect(checkboxElement).not.toBeChecked();
     expect(buttonElement).toBeDisabled();
   });
