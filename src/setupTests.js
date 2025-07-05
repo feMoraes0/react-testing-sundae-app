d2dbbc7c7d8e320e2c993e109e29739ebe417c80
feMoraes0 <fernandomoraes.lopes@gmail.com>
2025-07-05 20:53:15 +0100

    creates msw mock server file and adds to vitest setup

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,1 +1,9 @@
 import "@testing-library/jest-dom";
+
+// vitest.setup.ts
+import { beforeAll, afterEach, afterAll } from "vitest";
+import { server } from "./mocks/node.js";
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
