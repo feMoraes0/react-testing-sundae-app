8fd745ddcad0a151087fdc054882dc383414a150
feMoraes0 <fernandomoraes.lopes@gmail.com>
2025-07-05 17:57:03 +0100

    creates msw handlers file with /scoops route

---
+++ src/mocks/handlers.js
@@ -0,0 +1,10 @@
+import { http, HttpResponse } from 'msw'
+
+export const handlers = [
+  http.get('http://localhost:3000/scoops', () => {
+    return HttpResponse.json([
+      { name: "chocolate", imagePath: "/images/chocolate.png" },
+      { name: "vanilla", imagePath: "/images/vanilla.png" },
+    ])
+  }),
+]
