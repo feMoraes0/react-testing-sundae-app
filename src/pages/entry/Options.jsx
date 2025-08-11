8a3954ee6fc4672823b281ee241122217aee24df
feMoraes0 <fernandomoraes.lopes@gmail.com>
2025-08-11 16:35:11 +0100

    feat(options): adds base and scoop component

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -1,5 +1,31 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import ScoopOptions from './ScoopOptions';
+import { Row } from 'react-bootstrap';
+
 const Options = ({ optionType }) => {
-  return <div />;
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios.get(`http://localhost:3000/${optionType}`)
+    .then((response) => {
+      setItems(response.data);
+    }).catch(() => {
+      // TODO: handle error later
+    });
+  }, [optionType]);
+
+  // TODO: replace null with ToppingOption when available.
+  const ItemComponent = optionType === 'scoops' ? ScoopOptions : null;
+  const optionItems = items.map((item) => {
+    return <ItemComponent key={item.name} name={item.name} imagePath={item.imagePath}  />
+  })
+
+  return (
+    <Row>
+      {optionItems}
+    </Row>
+  );
 };
 
 export default Options;
