7b45b27ff32a717e73d55f4da16be224f0fa005d
feMoraes0 <fernandomoraes.lopes@gmail.com>
2025-08-11 16:35:22 +0100

    test(options): adds base and scoop component

--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -1,12 +1,10 @@
 import { screen, render } from "@testing-library/react";
 import Options from "../Options";
 
-test("displays image for each scoop option from server", () => {
-  render(<Options optionType="scoop" />);
-
-  const scoopImages = screen.getAllByRole("img", { name: /scoop$/i });
+test("displays image for each scoop option from server", async () => {
+  render(<Options optionType="scoops" />);
+  const scoopImages = await screen.findAllByRole("img", { name: /scoop$/i });
   expect(scoopImages).toHaveLength(2);
-
   const scoopAltTexts = scoopImages.map((element) => element.alt);
-  expect(scoopAltTexts).toEqual(["Chocolate scoop", "Vanilla scoop"]);
+  expect(scoopAltTexts).toEqual(["chocolate scoop", "vanilla scoop"]);
 });
