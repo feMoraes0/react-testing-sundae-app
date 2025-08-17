c38e060574014d178e77fc01db1c2c1f8984919f
feMoraes0 <fernandomoraes.lopes@gmail.com>
2025-08-17 16:45:58 +0100

    test: adds options parameter to testing library options returned render

--- src/test-utils/testing-library-utils.jsx
+++ src/test-utils/testing-library-utils.jsx
@@ -2,7 +2,7 @@ import { render } from "@testing-library/react";
 import { OrderDetailsProvider } from "../contexts/OrderDetails";
 
 const renderWithContext = (component, options) =>
-  render(component, { wrapper: OrderDetailsProvider });
+  render(component, { wrapper: OrderDetailsProvider, ...options });
 
 export * from "@testing-library/react";
 export { renderWithContext as render };
