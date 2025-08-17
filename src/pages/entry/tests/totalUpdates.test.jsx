8972871cf122b4c9c9da840935f12e6d6509e46a
feMoraes0 <fernandomoraes.lopes@gmail.com>
2025-08-17 15:05:40 +0100

    test(OrderDetails): creates test scenario with scoops total validation

---
+++ src/pages/entry/tests/totalUpdates.test.jsx
@@ -0,0 +1,26 @@
+import { test, expect } from "vitest";
+import { screen, render } from "../../../test-utils/testing-library-utils";
+import userEvent from "@testing-library/user-event";
+import Options from "../Options";
+
+test("update scoops total when scoops changes", async () => {
+  const user = userEvent.setup();
+  render(<Options optionType="scoops" />);
+  // make sure total starts out $0.00
+  const scoopSubtotal = screen.getByText("Scoops total: $", { exact: false });
+  expect(scoopSubtotal).toHaveTextContent("0.00");
+  // update vanilla scoops to 1 and check the subtotal
+  const vanillaInput = await screen.findByRole("spinbutton", {
+    name: "vanilla",
+  });
+  await user.clear(vanillaInput);
+  await user.type(vanillaInput, "1");
+  expect(scoopSubtotal).toHaveTextContent("2.00");
+  // update chocolate scoops to 2 and check subtotal
+  const chocoloateInput = await screen.findByRole("spinbutton", {
+    name: "chocolate",
+  });
+  await user.clear(chocoloateInput);
+  await user.type(chocoloateInput, "2");
+  expect(scoopSubtotal).toHaveTextContent("6.00");
+});
