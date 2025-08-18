56915ba1508a92265e21ee05a6760e5da8973d7e
feMoraes0 <fernandomoraes.lopes@gmail.com>
2025-08-18 18:23:06 +0100

    feat(OrderEntry): implements grand total and final page layout

--- src/pages/entry/OrderEntry.jsx
+++ src/pages/entry/OrderEntry.jsx
@@ -1,10 +1,15 @@
+import { useOrderDetails } from "../../contexts/OrderDetails";
+import { formatToCurrency } from "../../utilities";
 import Options from "./Options";
 
 const OrderEntry = () => {
+  const { totals } = useOrderDetails();
   return (
     <>
+      <h1>Design your sundae!</h1>
       <Options optionType="scoops" />
       <Options optionType="toppings" />
+      <h2>Grand total: {formatToCurrency(totals.scoops + totals.toppings)}</h2>
     </>
   );
 };
