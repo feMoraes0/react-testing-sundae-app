1f6e7af9e3831d1bcaa2e7d3702e394efc87f6d9
feMoraes0 <fernandomoraes.lopes@gmail.com>
2025-08-18 17:29:08 +0100

    feat(OrderDetails): implements topping options value calculation

--- src/pages/entry/ToppingOption.jsx
+++ src/pages/entry/ToppingOption.jsx
@@ -1,12 +1,22 @@
-import { Col } from "react-bootstrap";
+import { Col, Form } from "react-bootstrap";
+import { useOrderDetails } from "../../contexts/OrderDetails";
 
 const ToppingOption = ({ name, imagePath }) => {
+  const { updateItemCount } = useOrderDetails();
+  function handleCheckbox(event) {
+    updateItemCount(name, event.target.checked ? 1 : 0, "toppings");
+    return;
+  }
+
   return (
     <Col>
       <img
         src={`http://localhost:3000/${imagePath}`}
         alt={`${name} toppings`}
       />
+      <Form.Group controlId={`${name}-topping-checkbox`}>
+        <Form.Check type="checkbox" label={name} onChange={handleCheckbox} />
+      </Form.Group>
     </Col>
   );
 };
