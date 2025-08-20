31fcea2406bc299dca9e8936d08dcfb2793838a3
feMoraes0 <fernandomoraes.lopes@gmail.com>
2025-08-20 13:50:35 +0100

    feat(OrderFlow): implements all pages at the main app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
 import { Container } from "react-bootstrap";
 import { OrderDetailsProvider } from "./contexts/OrderDetails";
 import OrderEntry from "./pages/entry/OrderEntry";
+import OrderSummary from "./pages/summary/OrderSummary";
+import { useState } from "react";
+import OrderConfirmation from "./pages/confirmation/OrderConfirmation";
 
 function App() {
+  const [orderPhase, setOrderPhase] = useState("inProgress");
+  let Component = OrderEntry;
+  switch (orderPhase) {
+    case "inProgress":
+      Component = OrderEntry;
+      break;
+    case "review":
+      Component = OrderSummary;
+      break;
+    case "completed":
+      Component = OrderConfirmation;
+      break;
+    default:
+  }
+
   return (
     <Container>
       <OrderDetailsProvider>
-        <OrderEntry />
+        <Container>{<Component setOrderPhase={setOrderPhase} />}</Container>
       </OrderDetailsProvider>
     </Container>
   );
