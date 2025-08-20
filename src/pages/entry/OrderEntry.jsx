1c74539c7aada8e95a58cad06ec7df5b9efa224a
feMoraes0 <fernandomoraes.lopes@gmail.com>
2025-08-20 13:49:38 +0100

    feat(OrderFlow): updates order entry with order phase props

--- src/pages/entry/OrderEntry.jsx
+++ src/pages/entry/OrderEntry.jsx
@@ -1,15 +1,22 @@
+import { Button } from "react-bootstrap";
 import { useOrderDetails } from "../../contexts/OrderDetails";
 import { formatToCurrency } from "../../utilities";
 import Options from "./Options";
 
-const OrderEntry = () => {
+const OrderEntry = ({ setOrderPhase }) => {
   const { totals } = useOrderDetails();
+
+  function handleClick() {
+    setOrderPhase("review");
+  }
+
   return (
     <>
       <h1>Design your sundae!</h1>
       <Options optionType="scoops" />
       <Options optionType="toppings" />
       <h2>Grand total: {formatToCurrency(totals.scoops + totals.toppings)}</h2>
+      <Button onClick={handleClick}>Order sundae</Button>
     </>
   );
 };
